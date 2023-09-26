362deb1a8dc814951a94eff20b95af1182b5eb63
unknown <Ankitbadhani102@gmail.com>
2023-09-26 12:43:08 +0530

    changes

--- backend/Server.js
+++ backend/Server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: ['https://todo-app-frontend-two.vercel.app', 'https://another-allowed-origin.com'],}));
 
 mongoose
   .connect(process.env.MONGO_URI)
